3ebf3341c61683becb0cad1c1e7a04257abb516f
Michael Kibler <mbkibler0123@gmail.com>
2019-05-15 07:14:59 -0400

    Persisting data completely using Realm (up to date with lecture 257)

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
 @UIApplicationMain
@@ -19,60 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
-        
-
-//        let data = Data()
-//        data.name = "Michael"
-//        data.age = 25
-//
+//   print(Realm.Configuration.defaultConfiguration.fileURL) use this line of code to print to the console your realm file path
         
         do{
 
-            let realm = try Realm()
+            _ = try Realm()
             
-        }catch{
-            print("error, \(error)")
+        } catch {
+            print("error initialising new realm, \(error)")
             
         }
         
     
         return true
-    }
-
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        
-        self.saveContext()
-    }
-    
-    // MARK: - Core Data stack
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-     
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-              
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-              
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
     }
 
 
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -63,14 +63,6 @@ class TodoListViewController: UITableViewController {
             
             cell.accessoryType = item.done ? .checkmark : .none
             
-            
-            //        if item.done == true {
-            //            cell.accessoryType = .checkmark
-            //        } else {
-            //            cell.accessoryType = .none
-            //
-            //        }
-            
         }else{
             
             cell.textLabel?.text = "No items added"
